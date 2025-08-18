cfe2216b9c418118e5c994b102c88e6a7e688ccd
Kassahun Desta <sulamates@gmail.com>
2025-08-18 21:48:53 +0300

    educator change

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -3,9 +3,10 @@ import { clerkClient } from '@clerk/express';
 export const updateRoleToEducator = async(req,res)=>{
     try{
         const userId = req.auth.userId;
-        await clerkClient.users.updateUserMetadata(userId,{
-            publicMetadata:{
-                role:'educator'
+        await clerkClient.users.updateUserMetadata(userId, 
+            {
+                publicMetadata:{
+                    role:'educator'
             }
         });
         res.json({success:true, message:'You can publish your a course now'});
