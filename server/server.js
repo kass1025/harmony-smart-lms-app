c46ebfc7d22aab9bbc47cc3f9b67bb90a29d1c80
Kassahun Desta <sulamates@gmail.com>
2025-08-18 22:24:57 +0300

    sp

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(clerkMiddleware())
 app.get('/', (req, res)=>{res.send('API Working')});
 
 app.post('/clerk', express.json(), clerkWebhooks)
+
 app.use('/api/educator', express.json(), educatorRouter);
 
 //Port
