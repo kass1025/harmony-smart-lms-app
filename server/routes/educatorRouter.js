7a3431275e7e76a8db2d7df18878105f74b224f5
Kassahun Desta <sulamates@gmail.com>
2025-08-19 19:10:28 +0300

    comd

--- server/routes/educatorRouter.js
+++ server/routes/educatorRouter.js
@@ -1,13 +1,11 @@
 import express from 'express';
-import upload from '../configs/multer.js';
-import { addCourse, updateRoleToEducator } from '../controllers/educatorController.js';
-import { protectEducator } from '../middlewares/authMiddleware.js';
+import { updateRoleToEducator } from '../controllers/educatorController.js';
 
 const educatorRouter = express.Router();
 
 //Add Educator role
 educatorRouter.get('/update-role', updateRoleToEducator);
-educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse);
+//educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse);
 
 export default educatorRouter;
 
